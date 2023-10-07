3bd3c665943d326366ae6998314af502967eeacb
posk43 <petyaposk@gmail.com>
2023-10-07 12:04:13 +0000

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,10 +5,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = False
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
+ALLOWED_HOSTS = ['158.160.12.177', '127.0.0.1', 'localhost', 'solldartaski.ddns.net']
 
 # Application definition
 
@@ -103,7 +103,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ backend/settings.py
@@ -0,0 +1,2 @@
+# Вместо xxx.xxx.xxx.xxx укажите IP вашего сервера
+ALLOWED_HOSTS = ['158.160.12.177', '127.0.0.1', 'localhost']
